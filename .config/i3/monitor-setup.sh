6cdabb78be9aee7fcd4f96a0dda8c310b40a5a3d
druckdev <63563978+druckdev@users.noreply.github.com>
2020-06-25 23:20:08 +0200

    Multi monitor support
    When restarting i3 check for connected second monitor: scale it and
    restart polybar if there is one.
    Put polybar tray on the main monitor.
    Do not kill polybar and relaunch it when restarting i3, but restart it
    using ipc (much faster).

---
+++ .config/i3/monitor-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ 1 -lt "$(xrandr -q | grep " connected" | wc -l)" ]; then
+	# scale second monitor to 3200x1800 and put to the left
+	xrandr --output eDP1 --auto --pos 3200x0 --primary --output DP1 --auto --scale-from 3200x1800 --pos 0x0 --fb 6400x1800
+	killall -q polybar
+	while pgrep -x polybar >/dev/null; do sleep 1; done
+else
+	xrandr --output eDP1 --auto --output DP1 --off
+fi
+
+"${XDG_CONFIG_HOME:-$HOME/.config}/polybar/launch.sh" &
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,10 +3,16 @@
 # if there is no running instance
 if ! pgrep -ax polybar >/dev/null 2>&1; then
 	# launch Polybar on every monitor
-	# https://github.com/polybar/polybar/issues/763#issuecomment-392960721
+	# https://github.com/polybar/polybar/issues/763
+	primary="$(xrandr -q | grep primary | cut -d' ' -f1)"
 	for m in $(polybar --list-monitors | cut -d':' -f1); do
-		MONITOR=$m polybar --reload -c "$HOME/.config/polybar/config" main &
+		export TRAY_POS=none
+		[ "$m" != "$primary" ] || export TRAY_POS=right
+		export MONITOR="$m"
+		polybar --reload -c "${XDG_CONFIG_HOME:-$HOME/.config}/polybar/config" main &
 	done
 
 	echo "Polybar launched..."
+else
+	polybar-msg cmd restart
 fi
