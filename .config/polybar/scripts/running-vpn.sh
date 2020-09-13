d3433bc83edb3838d16094984ea75d1d08eecf59
druckdev <63563978+druckdev@users.noreply.github.com>
2020-09-13 04:45:15 +0200

    Small changes in vpn polybar module
    Check for tunnel connections now as well.
    Add exit code that signals if a VPN is used or not.

--- .config/polybar/scripts/running-vpn.sh
+++ .config/polybar/scripts/running-vpn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-connection="$(nmcli con show --active | awk '$3 == "vpn" { print $1 }')"
+connection="$(nmcli con show --active | awk '$3 ~ /^(vpn|tun)$/ { print $1 }')"
 if [ -n "$connection" ]; then
     echo "VPN: $connection"
     exit 0
@@ -8,6 +8,10 @@ fi
 
 # [o] is a hack to not grep the grep-command
 # see: https://stackoverflow.com/questions/9375711/more-elegant-ps-aux-grep-v-grep
-! ps ax | grep -q "[o]penvpn" || echo -n "VPN"
+if ps ax | grep -q "[o]penvpn"; then
+	echo "VPN"
+	exit 0
+fi
 
 echo
+exit 1
