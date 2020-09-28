1d3adc30e55d1215843a48c8de85ce5ddf9ca907
druckdev <63563978+druckdev@users.noreply.github.com>
2020-09-28 18:08:03 +0200

    polybar: Make hardcoded paths dependant from XDG
    Instead of hardcoding the paths of all the scripts, use env variables
    that are set in launch.sh. This way the config also works with
    XDG_CONFIG_HOME not set to ~/.config.
    Sadly polybar only accepts references that stand on their own.
    (So something like ${env:XDG_CONFIG_HOME:~/.config}/polybar/<script>
    does not work.) Hence the mess with the associative array and the
    variables for every module.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+XDG_CONF="${XDG_CONFIG_HOME:-$HOME/.config}"
+BASE_DIR="$XDG_CONF/polybar"
+
+declare -A module_flags
+module_flags=(
+	[battery]="$BASE_DIR/scripts/battery.sh"
+	[bluetooth]="$BASE_DIR/scripts/bluetooth.sh"
+	[bluetooth_click_left]="$BASE/scripts/bluetooth.sh --toggle &"
+	[media]="$BASE_DIR/scripts/media.sh"
+	[powermenu]="%{A1:$XDG_CONF/rofi/powermenu.sh &:}%{T2}...%{T-}%{A}"
+	[vpn]="$BASE_DIR/scripts/vpn.sh"
+	[vpn_click_left]="$BASE_DIR/scripts/pub_ipv4.sh &"
+)
+for module in "${!module_flags[@]}"; do
+	export POLYBAR_${module^^}="${module_flags[$module]}"
+done
 
 # if there is no running instance
 if ! pgrep -ax polybar >/dev/null 2>&1; then
@@ -9,7 +26,7 @@ if ! pgrep -ax polybar >/dev/null 2>&1; then
 		export TRAY_POS=none
 		[ "$m" != "$primary" ] || export TRAY_POS=right
 		export MONITOR="$m"
-		polybar --reload -c "${XDG_CONFIG_HOME:-$HOME/.config}/polybar/config" main &
+		polybar --reload -c "$BASE_DIR/config" main &
 	done
 
 	echo "Polybar launched..."
