555e983d1fedd84db766557eb8e34f5b10314896
druckdev <druckdev@protonmail.com>
2021-01-07 09:49:37 +0100

    zsh: Fix truncation of history by bash
    As bash uses some environment variables with the same name as zsh it
    uses the same history file and potentially truncates it, when there is
    no bashrc.

--- .config/zsh/zshrc.d/70-history.zsh
+++ .config/zsh/zshrc.d/70-history.zsh
@@ -1,5 +1,7 @@
 HISTSIZE=1000000
 SAVEHIST=1000000
+# Keep bash from truncating zsh's history if there is no bashrc
+export HISTFILESIZE=1000000
 
 if [[ ! -d "${XDG_DATA_HOME:-$HOME/.local/share}"/zsh ]]; then
 	mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}"/zsh
