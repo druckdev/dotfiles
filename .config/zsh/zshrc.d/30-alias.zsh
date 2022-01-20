2dcece04f347d8bc5423e6281d8dbdd9d385c3cf
druckdev <druckdev@protonmail.com>
2022-01-20 16:02:24 +0100

    zsh:alias: Add further git aliases

--- .config/zsh/zshrc.d/30-alias.zsh
+++ .config/zsh/zshrc.d/30-alias.zsh
@@ -58,11 +58,15 @@ fi
 	alias g='git'
 	alias gs='git status --short'
 	alias ga='git add'
+	alias gap='git add -p'
 	alias gc="git commit"
 	alias gpush='git push'
 	alias gpull='git pull'
 	alias gd='git diff'
 	alias gl='git log'
+	alias gr='git rebase'
+	alias grc='git rebase --continue'
+	alias grcia='git rebase --committer-date-is-author-date'
 	alias gss='git stash'
 	# https://nilansanjaya.wordpress.com/2017/06/02/git-find-base-branch/
 	git_bb='git show-branch -a 2>/dev/null'
