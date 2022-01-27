fb92842d0a676743881a898ef875ceda84be6096
druckdev <druckdev@protonmail.com>
2022-01-27 14:43:57 +0100

    zsh:autoload: Include all subdirs in `autoload/`
    Make it possible to organize autoloadable functions.

--- .config/zsh/zshrc.d/00-autoload.zsh
+++ .config/zsh/zshrc.d/00-autoload.zsh
@@ -7,6 +7,12 @@ autoload -Uz run-help run-help-git zmv
 
 # Load autoloadable functions
 if [[ -d "$ZDOTDIR/autoload" ]]; then
+	# Include all subdirectories
+	for d in "$ZDOTDIR/autoload/"*(/N); do
+		fpath=("$d" $fpath)
+		autoload -Uz -- "" "$d"/[^_.]*(.xN:t)
+	done
+
 	fpath=("$ZDOTDIR/autoload" $fpath)
 	autoload -Uz -- "" "${fpath[1]}"/[^_.]*(.xN:t)
 fi
