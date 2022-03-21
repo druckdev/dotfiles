druckdev/dotfiles

commit 41656d28d6e9d601a1a923b1dd517175b36b235b

vim:autocmd: Reset textwidth value in colorcolumn
Instead of overwriting the entire colorcolumn value, reset only the
textwidth value, by subtracting it first and then readding it.

.config/vim/vimrc.d/80-autocommands.vim
@@ -58,7 +58,7 @@ endif
 autocmd SourcePost termdebug.vim tnoremap <Esc> <C-\><C-n>
 
 " Adjust the colorcolumn together with &textwidth
-autocmd OptionSet textwidth let &colorcolumn=&textwidth
+autocmd OptionSet textwidth exec 'set cc=' . v:option_old . ' | set cc+=' . &tw
 "
 " Highlight word under cursor
 augroup highlight_current_word

commit 94271371dcb0beda6f20da7f604314543c21982f

vim:autocmd: Fix typo in `OptionSet textwidth`
The old `textwidth` value is meant to be subtracted from colorcolumn, to
then add the new one.

Fixes: 41656d28d6e9 ("vim:autocmd: Reset textwidth value in colorcolumn")

.config/vim/vimrc.d/80-autocommands.vim
@@ -58,7 +58,7 @@ endif
 autocmd SourcePost termdebug.vim tnoremap <Esc> <C-\><C-n>
 
 " Adjust the colorcolumn together with &textwidth
-autocmd OptionSet textwidth exec 'set cc=' . v:option_old . ' | set cc+=' . &tw
+autocmd OptionSet textwidth exec 'set cc-=' . v:option_old . ' | set cc+=' . &tw
 "
 " Highlight word under cursor
 augroup highlight_current_word