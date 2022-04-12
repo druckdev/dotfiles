ce4dae3b1a778f42c0a7ac6408cc3f7db2812426
Julian Prein <druckdev@protonmail.com>
2022-04-12 17:33:21 +0200

    zsh:comp: Complete insensitive between `-` and `_`

--- .config/zsh/zshrc.d/20-completion.zsh
+++ .config/zsh/zshrc.d/20-completion.zsh
@@ -12,7 +12,7 @@ zstyle ':completion:*' menu select=2
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
-zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{-_a-zA-Z}={_-A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
 zstyle ':completion:*' menu select=long
 zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
 zstyle ':completion:*' use-compctl false
