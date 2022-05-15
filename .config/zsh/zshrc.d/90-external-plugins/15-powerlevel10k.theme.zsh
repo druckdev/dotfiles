127cce45a1238abf7288d211cde9ac8c3583a6d4
Julian Prein <druckdev@protonmail.com>
2022-05-15 09:14:16 +0200

    zsh:p10k: Add vim modeline

--- .config/zsh/zshrc.d/90-external-plugins/15-powerlevel10k.theme.zsh
+++ .config/zsh/zshrc.d/90-external-plugins/15-powerlevel10k.theme.zsh
@@ -1616,3 +1616,5 @@ typeset -g POWERLEVEL9K_CONFIG_FILE=${${(%):-%x}:a}
 
 (( ${#p10k_config_opts} )) && setopt ${p10k_config_opts[@]}
 'builtin' 'unset' 'p10k_config_opts'
+
+# vim: ft=zsh tw=100 et ts=2
