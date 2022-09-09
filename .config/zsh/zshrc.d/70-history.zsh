2177031c68877089d42ac647df30819803c50fb3
Julian Prein <druckdev@protonmail.com>
2022-09-10 00:33:55 +0200

    zsh:history: Increase file size limit

--- .config/zsh/zshrc.d/70-history.zsh
+++ .config/zsh/zshrc.d/70-history.zsh
@@ -1,7 +1,8 @@
-HISTSIZE=1000000
-SAVEHIST=1000000
+HISTSIZE=999999999
+SAVEHIST="$HISTSIZE"
+
 # Keep bash from truncating zsh's history if there is no bashrc
-export HISTFILESIZE=1000000
+export HISTFILESIZE="$HISTSIZE"
 
 if [[ ! -d "${XDG_DATA_HOME:-$HOME/.local/share}"/zsh ]]; then
 	mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}"/zsh
