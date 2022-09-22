7666086dbaa60b9ec5c598e5c7c0ba18441a86dd
Julian Prein <druckdev@protonmail.com>
2022-09-22 14:29:35 +0200

    *: Move `die()` into .local/bin/helpers.sh
    The function `die` was redundantly implemented in various files.
    
    Move the function into .local/bin/helpers.sh and source that where
    previously implemented.
    
    Also prepend the program's name to the message and always terminate the
    message with a newline. The newline was previously needed for a small
    but unnecessary hack that prevented the need of the `[ -z "$1" ]` test.

---
+++ .local/bin/helpers.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# A collection of useful shell functions. This file should be sourced, rather
+# than executed.
+
+
+# Print error message, prepended by the programs name, and then exit
+#
+# Usage: die [<MESSAGE>] [<EXIT_CODE>]
+die() {
+	[ -z "$1" ] || >&2 printf "%s: %s\n" "$0" "$1"
+	exit ${2:-1}
+}
