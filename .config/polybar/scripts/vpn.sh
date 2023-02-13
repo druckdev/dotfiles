b489fdbe4bdaf36ebc9c154cc969c1ea1f6a9133
Julian Prein <druckdev@protonmail.com>
2023-02-13 16:39:04 +0100

    polybar:vpn: Parse mullvad status

--- .config/polybar/scripts/vpn.sh
+++ .config/polybar/scripts/vpn.sh
@@ -2,6 +2,9 @@
 
 connection="$(nmcli con show --active | awk '$3 ~ /^(vpn|tun|wireguard)$/ { print $1 }')"
 if [ -n "$connection" ]; then
+	if [ "$connection" = "wg-mullvad" ]; then
+		connection="$(mullvad status | awk '{ print $3}')"
+	fi
 	echo "VPN: $connection"
 	exit 0
 fi
