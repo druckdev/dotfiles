druckdev/dotfiles

commit 8096917ad421a5e4d9a7fc479508fbc91e0997fc

vim:keys: Open file in split with `<C-w>g[fF]`
Open the file under the cursor in a split window similar to the rest of
the `<C-w>` mappings. I'm surprised this mapping does not exist by
default.

.config/vim/vimrc.d/40-keys.vim
@@ -13,6 +13,10 @@ nnoremap <silent> <Esc> :nohlsearch <bar> call ClearHighlights()<CR><Esc>
 nnoremap <C-w>N <Cmd>vsplit<CR>
 nnoremap <C-w>n <Cmd>split<CR>
 
+" Open file under cursor in split
+nnoremap <C-w>gf <Cmd>split<CR>gf
+nnoremap <C-w>gF <Cmd>split<CR>gF
+
 " Substitute command
 if (exists('+inccommand') && &inccommand != '')
 	nnoremap S :%s/

commit a4405ee6142fbfcd99be1e862bc7b75189029442

vim:keys: Change family of <C-w>gf mappings
The mapping to split and open the file under cursor exists by default
(not as I thought), but is not <C-w>gf but rather <C-w>f.

Swap the mappings for tab and split 'destination'.

Fixes: 8096917ad ("vim:keys: Open file in split with `<C-w>g[fF]`")

.config/vim/vimrc.d/40-keys.vim
@@ -13,9 +13,13 @@ nnoremap <silent> <Esc> :nohlsearch <bar> call ClearHighlights()<CR><Esc>
 nnoremap <C-w>N <Cmd>vsplit<CR>
 nnoremap <C-w>n <Cmd>split<CR>
 
-" Open file under cursor in split
-nnoremap <C-w>gf <Cmd>split<CR>gf
-nnoremap <C-w>gF <Cmd>split<CR>gF
+" My brain expects <C-w>{gf,gF} to open in a split, not a tab.
+" Swap the mappings for tab and split.
+nnoremap <C-w>gf <C-w>f
+nnoremap <C-w>gF <C-w>F
+nnoremap <C-w>f <C-w>gf
+nnoremap <C-w><C-f> <C-w>gf
+nnoremap <C-w>F <C-w>gF
 
 " Substitute command
 if (exists('+inccommand') && &inccommand != '')