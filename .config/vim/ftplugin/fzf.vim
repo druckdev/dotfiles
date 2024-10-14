druckdev/dotfiles

commit 86af2da785d77c70c4d58b4c2bdeaf354f97ff5d

vim:ftplug:fzf:TODO: Look up other Unicode chars

.config/vim/ftplugin/fzf.vim
@@ -1,0 +1,5 @@
+" Do not highlight Unicode box drawing chars as non-ascii
+" TODO: Look into fzf source code for all other possible Unicode chars
+let w:ignore_non_ascii_chars = '─│╭╮╰╯▌' .. get(w:, 'ignore_non_ascii_chars', '')
+" Update after changes
+call HighlightNonASCIIChars()

commit 8a101134c7b0633c930f8917b9563cb987cf84c8

vim:ftplug:fzf: Complete list of TUI Unicode chars
This excludes more characters than needed.

Fixes: 86af2da785d7 ("vim:ftplug:fzf:TODO: Look up other Unicode chars")

.config/vim/ftplugin/fzf.vim
@@ -1,5 +1,15 @@
-" Do not highlight Unicode box drawing chars as non-ascii
-" TODO: Look into fzf source code for all other possible Unicode chars
-let w:ignore_non_ascii_chars = '─│╭╮╰╯▌┃' .. get(w:, 'ignore_non_ascii_chars', '')
+let w:ignore_non_ascii_chars = get(w:, 'ignore_non_ascii_chars', '')
+
+" Do not highlight Unicode chars used for the TUI
+" Unicode "Box Drawing" block
+let w:ignore_non_ascii_chars ..= '\u2500-\u257f'
+" Unicode "Block Elements" block
+let w:ignore_non_ascii_chars ..= '\u2580-\u259f'
+" Unicode "Block elements" subblock of the "Symbols for Legacy Computing" block
+" (i.e. 1/8th block symbols)
+let w:ignore_non_ascii_chars ..= '\U0001fb70-\U0001fb89'
+" Braille symbols for spinner
+let w:ignore_non_ascii_chars ..= '⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏'
+
 " Update after changes
 call HighlightNonASCIIChars()