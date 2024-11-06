druckdev/dotfiles

commit 658797bda246de20c82b6f5c691b2f723e8fcb3c

zsh:keys: Use `vi-` variants of navigation binds
They seem to be more consistent in doing the same/reversed thing when
triggering them backwards.

.config/zsh/zshrc.d/60-keys.zsh
@@ -49,21 +49,21 @@ bindkey '^E' edit-command-line
 bindkey '^S' vi-pound-insert
 
 ## Navigation
-bindkey '^[[Z' reverse-menu-complete         # shift-tab
-bindkey '^Q' push-input                      # ctrl-Q
-bindkey '^[[H' beginning-of-line             # home
-bindkey "$terminfo[khome]" beginning-of-line # home
-bindkey '^[[F' end-of-line                   # end
-bindkey "$terminfo[kend]" end-of-line        # end
-bindkey -v '^?' backward-delete-char         # normal delete not vim-bac...
-bindkey '^[[P' delete-char                   # delete
-bindkey '^[[3~' delete-char                  # delete
-bindkey '^[[1;5D' backward-word              # ctrl-left
-bindkey '^[[1;5C' forward-word               # ctrl-right
-bindkey '^W' backward-kill-word              # ctrl-W
-bindkey '^H' backward-kill-word              # ctrl-backspace
-bindkey '^[[3;5~' kill-word                  # ctrl-delete
-bindkey "$terminfo[kmous]" kill-word         # ctrl-delete
+bindkey '^[[Z' reverse-menu-complete            # shift-tab
+bindkey '^Q' push-input                         # ctrl-Q
+bindkey '^[[H' vi-beginning-of-line             # home
+bindkey "$terminfo[khome]" vi-beginning-of-line # home
+bindkey '^[[F' vi-end-of-line                   # end
+bindkey "$terminfo[kend]" vi-end-of-line        # end
+bindkey '^?' vi-backward-delete-char            # backspace
+bindkey '^[[P' vi-delete-char                   # delete
+bindkey '^[[3~' vi-delete-char                  # delete
+bindkey '^[[1;5D' vi-backward-word              # ctrl-left
+bindkey '^[[1;5C' vi-forward-word               # ctrl-right
+bindkey '^W' vi-backward-kill-word              # ctrl-W
+bindkey '^H' vi-backward-kill-word              # ctrl-backspace
+bindkey '^[[3;5~' vi-kill-word                  # ctrl-delete
+bindkey "$terminfo[kmous]" vi-kill-word         # ctrl-delete
 
 # Open file in EDITOR selected with fzf
 function edit-fuzzy-file {

commit 9271350aab1e849f74ca8fd47b47d28e700948bc

zsh:keys: Fix non-existing mapping of vi-kill-word
vi-kill-word does not exist. This change was lying around uncommitted
for ages now.

Fixes: 658797bda246 ("zsh:keys: Use `vi-` variants of navigation binds")

.config/zsh/zshrc.d/60-keys.zsh
@@ -69,7 +69,7 @@ bindkey '^[[127;5u' backward-kill-word           # ctrl-backspace
 bindkey '^W' backward-kill-word                  # ctrl-W
 bindkey '^[[1;5D' vi-backward-word               # ctrl-left
 bindkey '^[[1;5C' vi-forward-word                # ctrl-right
-bindkey '^[[3;5~' vi-kill-word                   # ctrl-delete
+bindkey '^[[3;5~' kill-word                      # ctrl-delete
 bindkey '^Q' push-input                          # ctrl-Q
 
 # cd-{rotate,backward,forward} and redraw-prompt are partially modified copies