f75130ea734843b6aa7c8d983b280dd145a2e876
Julian Prein <julian@druck.dev>
2025-05-29 17:34:14 +0200

    zotero: Add user.js that sets recursiveCollections

---
+++ .config/zotero/user.js
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Julian Prein
+//
+// The symlink in Zotero's profile has to be created manually:
+// ~/.zotero/zotero/*/user.js -> ~/.config/zotero/user.js
+
+user_pref("extensions.zotero.recursiveCollections", true);
