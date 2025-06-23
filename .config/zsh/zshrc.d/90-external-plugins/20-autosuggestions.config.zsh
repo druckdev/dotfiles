4b5dbd161a7a124b997e5394abe30d357e87e966
Julian Prein <julian@druck.dev>
2025-06-24 00:06:33 +0200

    zsh:autosuggestions: Switch to history strategy
    I don't feel as if I'm actively benefiting from match_prev_cmd and it
    definitely makes my prompt slower. (One could probably make it faster by
    pre-calculating the history events matching the (previously) executed
    command in a preexec zsh-hook and then comparing each successor to the
    currently typed command instead of repeating all the work every time
    BUFFER changes)
    
    With this change my fork is not needed anymore, so switch back to
    zsh-users' repo and update it while at it.
    
    This effectively reverts 77b2d63341e7 ("zsh:autosuggestions: Use own
    fork for now").

--- .config/zsh/zshrc.d/90-external-plugins/20-autosuggestions.config.zsh
+++ .config/zsh/zshrc.d/90-external-plugins/20-autosuggestions.config.zsh
@@ -1,13 +1,8 @@
 # Set suggestions strategy
-# match_prev_cmd:
-#     Chooses the most recent match from history whose preceding item matches
-#     the most recently executed command.
-#     Won't work as expected with HIST_IGNORE_ALL_DUPS or HIST_EXPIRE_DUPS_FIRST
+# history:
+#     Chooses the most recent match from history.
 # completion:
 #     Chooses a suggestion based on what tab-completion would suggest.
-ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd completion)
-
-ZSH_AUTOSUGGEST_MATCH_NUM_PREV_CMDS=2
-ZSH_AUTOSUGGEST_MATCH_PREV_MAX_CMDS=-1
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
 # vim: ft=zsh
