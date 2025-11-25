druckdev/dotfiles

commit 558bb0582e82b00a8f9078e4dd47942b91fd78fa

man.vim: Check if WinResized is supported
WinResized was introduced rather recently with vim patch 9.0.0917 /
neovim v0.9.0 via commit 4571ba4d0a52 ("vim-patch:partial:9.0.0917: the
WinScrolled autocommand event is not enough (#21161)").

.config/vim/after/ftplugin/man.vim
@@ -33,6 +33,9 @@ normal M
 "
 " This is very hacky.
 
+" Only if WinResized is supported
+if has('##WinResized')
+
 augroup man_resized
 	" The reload has to be delayed slightly, since with an `edit` directly in
 	" the autocmd, the buffer will just be cleared? (TODO)
@@ -65,4 +68,6 @@ endif
 " When wrapping is turned on, shrinking the window will momentarily rewrap the
 " lines, messing up the whole buffer. Since this is distracting, turn it off.
 setlocal nowrap
+
+endif " has('##WinResized')
 " ------------------------------------------------------------------------------

commit ead724b75ba0e18e3960d8580fdc12cf3b637652

vim:ftplug:man: Fix WinResized support check
The check if the WinResized event is supported was broken from the
beginning on since I used `has()` instead of `exists()`.

Fixes: 558bb0582e82 (man.vim: Check if WinResized is supported,
       2025-01-29)

.config/vim/after/ftplugin/man.vim
@@ -37,7 +37,7 @@ setlocal showbreak=NONE
 " This is very hacky.
 
 " Only if WinResized is supported
-if has('##WinResized')
+if exists('##WinResized')
 
 augroup man_resized
 	" The reload has to be delayed slightly, since with an `edit` directly in
@@ -72,5 +72,5 @@ endif
 " lines, messing up the whole buffer. Since this is distracting, turn it off.
 setlocal nowrap
 
-endif " has('##WinResized')
+endif " exists('##WinResized')
 " ------------------------------------------------------------------------------