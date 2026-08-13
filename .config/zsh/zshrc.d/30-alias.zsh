druckdev/dotfiles

commit 40f8c94b4ce4bd7b751e20abc1590b99150d21af

zsh:alias: Fix missing quotes in `sizes`

.config/zsh/zshrc.d/30-alias.zsh
@@ -128,7 +128,7 @@ fi
 	# Use a reasonable time format
 	alias date='env LC_TIME=tk_TM date'
 	# List human readable sizes in order
-	alias sizes='() { du -sch ${1:-*} "${@[2,-1]}" | sort -h }'
+	alias sizes='() { du -sch "${1:-*}" "${@[2,-1]}" | sort -h }'
 	# Count number of occurrences for every line in stdin
 	alias count='sort | uniq -c | sort -n'
 	# Inspired by https://stackoverflow.com/a/54541337

commit 2e921161cc7519db13609639c78683f9ac7518c9

zsh:alias: Fix quotation of fallback glob
Commit 40f8c94b4ce4 (zsh:alias: Fix missing quotes in `sizes`,
2026-08-05) fixed the missing quotes for the optional first argument but
at the same time broke the globbing fallback, since it has to be
unquoted.

I tried to come up with a nice solution using the `(q)` parameter
expansion flag that doesn't require the two cases, but failed doing so.
The problem is that the empty string is escaped as an empty pair of
quotes and thus breaks the non-null check. One could use a substitution
but that seemed less readable than how it is now.

Fixes: 40f8c94b4ce4 (zsh:alias: Fix missing quotes in `sizes`,
       2026-08-05)

.config/zsh/zshrc.d/30-alias.zsh
@@ -128,7 +128,7 @@ fi
 	# Use a reasonable time format
 	alias date='env LC_TIME=tk_TM date'
 	# List human readable sizes in order
-	alias sizes='() { du -sch "${1:-*}" "${@[2,-1]}" | sort -h }'
+	alias sizes='() { { ((#)) && du -sch "$@" || du -sch * } | sort -h }'
 	# Count number of occurrences for every line in stdin
 	alias count='sort | uniq -c | sort -n'
 	# Inspired by https://stackoverflow.com/a/54541337